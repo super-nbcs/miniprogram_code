8835483ae82f73c039ee05e1240cdbfc1e5dc086
zfw <718059569@qq.com>
2020-12-21 11:18:47 +0800

    init_sql

--- core/src/main/resources/sql/init_sql.sql
+++ core/src/main/resources/sql/init_sql.sql
@@ -8,8 +8,6 @@ INSERT INTO sys_role (id,create_by,create_date,del_flag,update_by,update_date,co
 INSERT INTO sys_user_role(id,create_by,create_date,del_flag,update_by,update_date,roleId,userId) VALUES (1,1,NOW(),0,1,NOW(),1,1);
 
 -- 初始化组织架构表
-
--- 初始化组织架构表
 INSERT INTO sys_dept(create_by, create_date, del_flag, remarks, sort, update_by, update_date, child_ids, code, dtl, email, flag, leader, loc, name, parent_id, phone, path, level, color, ops_server) VALUES ('1', '2020-08-30 03:01:46', '0', NULL, NULL, '1', '2020-08-30 03:01:46', NULL, '10', NULL, NULL, '0', NULL, NULL, '管理系统', '0', NULL, '/管理系统', '1', '#000000', NULL);
 INSERT INTO sys_dept(create_by, create_date, del_flag, remarks, sort, update_by, update_date, child_ids, code, dtl, email, flag, leader, loc, name, parent_id, phone, path, level, color, ops_server) VALUES ('1', '2020-08-30 03:01:46', '0', NULL, NULL, '1', '2020-08-30 03:01:46', NULL, '1010', NULL, NULL, '0', NULL, NULL, '微信', '1', NULL, '/管理系统/微信', '2', '#000000', NULL);
 INSERT INTO sys_dept(create_by, create_date, del_flag, remarks, sort, update_by, update_date, child_ids, code, dtl, email, flag, leader, loc, name, parent_id, phone, path, level, color, ops_server) VALUES ('1', '2020-08-30 03:01:46', '0', NULL, NULL, '1', '2020-08-30 03:01:46', NULL, '101010', NULL, NULL, '0', NULL, NULL, '小程序', '2', NULL, '/管理系统/微信/小程序', '2', '#000000', NULL);
