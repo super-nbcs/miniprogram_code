5c3733fe07b9107827b1f507cfe5e24c4abb1a23
zfw <718059569@qq.com>
2020-12-23 14:56:04 +0800

    测试base类

---
+++ start/src/test/java/com/zfw/test/BaseServerTest.java
@@ -0,0 +1,17 @@
+package com.zfw.test;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * @Author:zfw
+ * @Date:2019/7/20
+ * @Content:
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@Transactional
+public class BaseServerTest {
+}
