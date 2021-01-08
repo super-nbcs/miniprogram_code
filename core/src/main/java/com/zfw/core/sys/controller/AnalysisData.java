a67286cb792497b4c9edf2472b90d0296e47dbe5
zfw <718059569@qq.com>
2021-01-08 17:12:54 +0800

    断言判断null值和空值

--- core/src/main/java/com/zfw/core/sys/controller/AnalysisData.java
+++ core/src/main/java/com/zfw/core/sys/controller/AnalysisData.java
@@ -2,7 +2,11 @@ package com.zfw.core.sys.controller;
 
 import com.alibaba.fastjson.JSONObject;
 import com.zfw.core.constant.Constant;
+import com.zfw.core.exception.GlobalException;
 import com.zfw.utils.ReturnUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Optional;
 
 /**
  * @Author:zfw
@@ -29,5 +33,10 @@ public interface AnalysisData {
 
     default JSONObject fail(Object data) {
         return ReturnUtils.fail(data);
+    }
+    default AnalysisData assertBlank(Object o, String msg) {
+        Optional.ofNullable(o).orElseThrow(() -> new GlobalException(msg));
+        if (o instanceof String && StringUtils.isBlank((CharSequence) o)) throw new GlobalException(msg);
+        return this;
     }
 }
